3c92c70d95bc8bce96e01fcdb1b02c00c9ca9e64
Flávio Tonon <flavioltonon@gmail.com>
2023-09-25 22:22:14 -0300

    implement zap json logger

---
+++ logging/zap/json/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"poc/shared/generic"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	encoder := zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+		LevelKey:   "level",
+		MessageKey: "msg",
+		// Encode log level as its string value in uppercase
+		EncodeLevel: zapcore.CapitalLevelEncoder,
+		// Encode log timestamp as its RFC3339Nano string value
+		EncodeTime: zapcore.RFC3339NanoTimeEncoder,
+		// Encode time.Duration fields as their string values
+		EncodeDuration: func(d time.Duration, pae zapcore.PrimitiveArrayEncoder) { pae.AppendString(d.String()) },
+	})
+
+	core := zapcore.NewCore(encoder, zapcore.AddSync(os.Stderr), zapcore.DebugLevel)
+
+	logger := zap.New(core,
+		zap.Fields(
+			zap.String("default", "banana"),
+
+			// Sets a "time" key with a custom value
+			zap.Time("time", generic.Time),
+		),
+	)
+
+	// {"level":"DEBUG","time":"2023-08-25T12:34:56.789101112Z","msg":"logging at DEBUG level","default":"banana"}
+	logger.Debug("logging at DEBUG level")
+
+	// {"level":"INFO","time":"2023-08-25T12:34:56.789101112Z","msg":"logging at INFO level","default":"banana","map":{"foo":"bar","baz":1}}
+	logger.Info("logging at INFO level", zap.Dict("map", zap.String("foo", "bar"), zap.Int("baz", 1)))
+
+	// {"level":"WARN","time":"2023-08-25T12:34:56.789101112Z","msg":"logging at WARN level","default":"banana","foo":"1h23m45s"}
+	logger.Warn("logging at WARN level", zap.Duration("foo", generic.Duration))
+
+	// {"level":"ERROR","time":"2023-08-25T12:34:56.789101112Z","msg":"logging at ERROR level","default":"banana","error":"some error"}
+	logger.Error("logging at ERROR level", zap.Any("error", generic.Error))
+}
