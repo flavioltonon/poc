ca8d8ebf612e8056e77a6ecb9204853b2e0e7757
Flávio Tonon <flavioltonon@gmail.com>
2023-10-12 11:30:25 -0300

    docs: improve template package documentation

--- shared/template/template.go
+++ shared/template/template.go
@@ -5,12 +5,13 @@ import (
 	"os"
 )
 
+// Template is a string template. It may contain variable values represented as ${var} or $var that can be
+// later replaced with values via Build method.
 type Template struct {
 	content string
 }
 
-type Fields map[string]string
-
+// FromFile creates a Template from a given file.
 func FromFile(path string) (*Template, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -29,6 +30,16 @@ func FromFile(path string) (*Template, error) {
 	}, nil
 }
 
+// Content returns the raw content of the Template. Can be used safely in case the template source does not
+// contain any variable values.
+func (t *Template) Content() string {
+	return t.content
+}
+
+type Fields map[string]string
+
+// Build replaces all variable values in the template string with the fields provided. Variables not mapped in
+// the fields should be replaced with empty strings.
 func (t *Template) Build(fields Fields) string {
 	return os.Expand(t.content, func(s string) string {
 		return fields[s]
