d533b5124cb2eee1a0cc265f472e0850f425024f
Flávio Tonon <flavioltonon@gmail.com>
2023-11-14 19:02:04 -0300

    feat: add golang.org/x/time/rate.Limiter example

--- go.mod
+++ go.mod
@@ -62,6 +62,7 @@ require (
 	golang.org/x/oauth2 v0.8.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20230530153820-e85fd2cbaebc // indirect
---
+++ throttling/rate/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"poc/shared/throughput"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+var (
+	limit = rate.Limit(1 * time.Second)
+	burst = 1
+)
+
+func main() {
+	limiter := rate.NewLimiter(limit, burst)
+
+	calculator := throughput.NewCalculator()
+	go calculator.Calculate(1 * time.Second /* every */)
+
+	ctx := context.Background()
+
+	for {
+		_ = limiter.Wait(ctx)
+		calculator.Increment()
+	}
+}
