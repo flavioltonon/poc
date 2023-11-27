6779d9969e03d8d61c85b0dad6ca404afe1f0e49
Flávio Tonon <flavio.tonon@unico.io>
2023-11-27 16:30:45 -0300

    chore: move rate.Limiter.Wait example

--- throttling/rate/wait/main.go
+++ throttling/rate/wait/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"poc/shared/throughput"
 	"time"
 
@@ -22,7 +23,13 @@ func main() {
 	ctx := context.Background()
 
 	for {
-		_ = limiter.Wait(ctx)
+		// Wait returns an error when the context it cancelled or if there are no tokens in the bucket (e.g. when
+		// the limiter's limit is set as zero in the runtime).
+		if err := limiter.Wait(ctx); err != nil {
+			log.Println(err)
+			continue
+		}
+
 		calculator.Increment()
 	}
 }
